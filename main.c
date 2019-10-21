8343280d967e6ea4e6db2c36d24432a8a9acbcd0
kressafox <56598590+kressafox@users.noreply.github.com>
2019-10-21 08:59:47 -0600

    File I/O supported

--- main.c
+++ main.c
@@ -5,9 +5,14 @@
 
 int main(int argc, char* argv[])
 {
-
+  FILE *outFile;
+  yyin = fopen("input.txt", "r");
+  outFile = fopen("output.txt" , "a");
+  dup2(fileno(outFile),1);
   yyparse();
-
+  
+  fclose(yyin);
+  fclose(outFile);
   return 0;
 
 }
