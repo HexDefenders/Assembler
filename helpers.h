86a2807ec7991531910c2067b510f4a8f7e18784
kressafox <56598590+kressafox@users.noreply.github.com>
2019-10-23 13:02:48 -0600

    label implementation for helpers.h

---
+++ helpers.h
@@ -0,0 +1,15 @@
+struct Label {
+  char *name;
+  int location;
+};
+
+extern int trigger;
+extern struct Label heldLoc[];
+extern int pc;
+extern int first_enter;
+
+void toLE(char *);
+char iToC(int);
+char* toHex(char *, int, int);
+int needLabel(char*);
+void holdLabel(char*, int );
